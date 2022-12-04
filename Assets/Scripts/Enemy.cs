75ad5f88811e0d3b12710e7808a48e8a1f3558d2
ivoshet <ivan.v.shet@gmail.com>
2022-12-04 19:34:10 +0500

    added destroyable for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    bool isHit = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,10 +22,28 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && !isHit)
         {
             collision.gameObject.GetComponent<Player>().RecountHp(-1);
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 8f, ForceMode2D.Impulse);
         }
+    }
+
+    public IEnumerator Death()
+    {
+        isHit = true;
+        print("death");
+        GetComponent<Animator>().SetBool("death", true);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        GetComponent<Collider2D>().enabled = false;
+        //GetComponentInChildren<Collider2D>().enabled = false;
+        transform.GetChild(0).GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+    }
+
+    public void StartDeath()
+    {
+        StartCoroutine(Death());
     }
 }
