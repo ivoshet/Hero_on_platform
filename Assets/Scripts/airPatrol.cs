87142d8e1b5be296f0ff0af2810862c65a2b087e
ivoshet <ivan.v.shet@gmail.com>
2022-12-04 10:22:18 +0500

    added rotation angle to bee

--- Assets/Scripts/airPatrol.cs
+++ Assets/Scripts/airPatrol.cs
@@ -38,6 +38,13 @@ public class airPatrol : MonoBehaviour
     IEnumerator Waiting()
     {
         yield return new WaitForSeconds(waitTime);
+        if (transform.rotation.y == 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        //transform.eulerAngles = new Vector3(0, transform.rotation.y + 180, 0);
         canGo = true;
     }
 
